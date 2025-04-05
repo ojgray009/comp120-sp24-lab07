9a22cf5f79458a37ca945ea330187de0f36dcf8a
Owen Gray <ogray@sandiego.edu>
2025-04-04 18:22:54 -0700

    u

--- linked_list.py
+++ linked_list.py
@@ -14,7 +14,7 @@ class Node:
     >>> n.data
     7
     >>> n.next
-    None
+    
     >>> m = Node(13, n)
     >>> m.data
     13
@@ -115,6 +115,87 @@ class LinkedList:
             current_other = current_other.next
 
         return current_self == current_other
+    
+    def get_item(self, index: int) -> Any:
+        """
+        Return the item at position <index> in this list.
+
+        Precondition: index >= 0.
+
+        Raises:
+        (IndexError) if the <index> is out of bounds.
+
+        Returns:
+        (Any) The data value stored at the given index in the list.
+
+
+        DocTest:
+
+        >>> l = LinkedList()
+        >>> l.add(2)
+        >>> l.add(1)
+        >>> l.get_item(0)
+        1
+        >>> l.get_item(1)
+        2
+        """
+        
+        curr = self.head 
+        i = 0
+        while curr is not None:
+            if i == index:  #stopping condition
+                return curr.data
+            curr = curr.next
+            i += 1
+        raise IndexError("out of range") #raises error to prevent range issues
+
+    def insert(self, index: int, item: Any) -> None:
+        """Insert the given item at the given index in this list.
+
+        Note that adding to the end of the list is okay.
+
+        Raises:
+            (IndexError) if index > len(self) or index < 0.
+
+        Args:
+            index (int): The index where the item will be inserted.
+            item (Any): The data/contents of the item to be inserted.
+
+        DocTest:
+        >>> l = LinkedList()
+        >>> l.insert(0, "first")  
+        >>> l.get_item(0)
+        'first'
+        >>> l.size()
+        1
+        >>> l.insert(0, "first2")  
+        >>> l.get_item(0)
+        'first2'
+        >>> l.get_item(1)
+        'first'
+        """
+        
+        if index < 0 or index > self.size():
+            raise IndexError("out of range")
+            
+        new_node = Node(item, self.head)
+        
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+            return
+            
+        curr = self.head
+        i = 0
+        while curr is not None and i < index - 1:
+            curr = curr.next
+            i += 1
+            
+        if curr is None: 
+            raise IndexError("out of range")
+            
+        new_node.next = curr.next
+        curr.next = new_node
 
 
 def create_random_linked_list(size: int, seed_val: int = 42) -> LinkedList:
